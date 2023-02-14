53e7e4a04e4b98d76706d314a55e3b6dea760587
Landon Krusniak <Lakrusniak@gnail.com>
2023-02-14 05:32:24 -0800

    final Blue

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -6,25 +6,27 @@
 */
 async function StartMaze() {
 	await speak("StartMaze", true);
-	setSpeed(60);
-	await delay (2);
-	setSpeed(0);
+	setSpeed(50);
+	await delay (1);
+	setSpeed(5);
 }
 /**
  * ninetyDegree function goes forward for a specified amount of time and speed
  * pausing, turning 90 degrees clockwise then moving forward for a specific amount
  * of speed and time.
 */
-async function oninetyDegree() {
-	await setMainLed({ r: 255, g: 0, b: 0});
-	await roll((getHeading() + 0), 60, 2); // Heading 0 degrees at 60 speed for 4 seconds
+async function oninetyDegree1() {
+	await setMainLed({ r:0 , g: 0, b: 255});
+	await roll((getHeading() + 0), 50, 2); // Heading 0 degrees at 60 speed for 4 seconds
 	await delay(1);
-	await roll((getHeading() + 90), 60, 2); // heading 90 degrees at 60 speed for 4 seconds
+	await roll((getHeading() + 90), 50, 2); // heading 90 degrees at 60 speed for 4 seconds
 }
+
 
 /**
  * startProgram function sends the program to your Sphero Bolt Maze Comp
 */
 async function startProgram() {
 	await StartMaze()
+	 
 }
