fb787aadba37b79a5e6567d862802c72f499de0c
Landon Krusniak <Lakrusniak@gnail.com>
2023-02-15 05:29:38 -0800

    red is good starting the next part 2-15-2023

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -2,20 +2,14 @@
 //Date:2.13.2023
 //Project:Spherp Bolt Maze
 
-/**Speak Start maze forward for 2 seconds a speed of 60
-*/
-async function StartMaze() {
-	await speak("StartMaze", true);
-	setSpeed(50);
-	await delay (1);
-	setSpeed(5);
-}
+
 /**
  * ninetyDegree function goes forward for a specified amount of time and speed
  * pausing, turning 90 degrees clockwise then moving forward for a specific amount
  * of speed and time.
 */
 async function oninetyDegree1() {
+	    await speak("StartMaze", true);
 	    await roll ((getHeading() + 0), 73, 2)
 	    await spin(90, 2)
 	    setMainLed({ r: 0, g: 0, b: 255 })
@@ -39,13 +33,18 @@ async function red(){
     setMainLed({ r: 225, g: 0, b: 0 })
     await roll ((getHeading() + 0), 25, 2)
 }
+async function sound2(){
+    	await roll ((getHeading() + 0), 38, 2)
+    	await spin (45,2)
+    	await roll ((getHeading() + 0), 42, 2)
 
+}
 
 /**
  * startProgram function sends the program to your Sphero Bolt Maze Comp
 */
 async function startProgram() {
-	await StartMaze()
+	
 	await oninetyDegree1()
 	await onenetyDegree2()
 	await red()
